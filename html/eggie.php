90843c1dcf0beb127dd05cfae9e07ece683f6ac5
Petr Bena <benapetr@gmail.com>
2014-07-30 14:25:01 +0200

    some new stuff

---
+++ html/eggie.php
@@ -0,0 +1,16 @@
+<?
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+// you may want to comment out this on production setup
+ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
+error_reporting(-1);
+require('eggie_main.php');
